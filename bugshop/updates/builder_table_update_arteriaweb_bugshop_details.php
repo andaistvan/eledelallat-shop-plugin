3436069c7fc8c2aaf81f7f55a4b798270b9b541a
andaistvan <andaistvan@gmail.com>
2017-03-05 20:33:12 +0100

    BUGSHOP plugin start

---
+++ bugshop/updates/builder_table_update_arteriaweb_bugshop_details.php
@@ -0,0 +1,23 @@
+<?php namespace Arteriaweb\Bugshop\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateArteriawebBugshopDetails extends Migration
+{
+    public function up()
+    {
+        Schema::table('arteriaweb_bugshop_details', function($table)
+        {
+            $table->increments('id')->unsigned(false)->change();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('arteriaweb_bugshop_details', function($table)
+        {
+            $table->increments('id')->unsigned()->change();
+        });
+    }
+}
