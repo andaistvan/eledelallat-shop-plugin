c8fdaad6ec1300d144ccdb8d941840aec4a12f25
andaistvan <andaistvan@gmail.com>
2017-03-09 05:16:31 +0100

    TABLES DONE

---
+++ bugshop/updates/builder_table_create_arteriaweb_bugshop_helpers.php
@@ -0,0 +1,24 @@
+<?php namespace Arteriaweb\Bugshop\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateArteriawebBugshopHelpers extends Migration
+{
+    public function up()
+    {
+        Schema::create('arteriaweb_bugshop_helpers', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('type');
+            $table->text('type_name');
+            $table->string('slug');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('arteriaweb_bugshop_helpers');
+    }
+}
