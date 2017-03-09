3c86b3893e0845e6b8c3b07f5eb4284138d96ba9
andaistvan <andaistvan@gmail.com>
2017-03-09 13:33:37 +0100

    RESTART
    BASIC tables:
    - anmials
    - datas

---
+++ bugshop/updates/builder_table_create_arteriaweb_bugshop_animal.php
@@ -0,0 +1,24 @@
+<?php namespace Arteriaweb\Bugshop\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateArteriawebBugshopAnimal extends Migration
+{
+    public function up()
+    {
+        Schema::create('arteriaweb_bugshop_animal', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('name');
+            $table->text('slug');
+            $table->string('description');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('arteriaweb_bugshop_animal');
+    }
+}
---
+++ bugshop/updates/builder_table_create_arteriaweb_bugshop_data.php
@@ -0,0 +1,24 @@
+<?php namespace Arteriaweb\Bugshop\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateArteriawebBugshopData extends Migration
+{
+    public function up()
+    {
+        Schema::create('arteriaweb_bugshop_data', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('datatype');
+            $table->string('dataname');
+            $table->string('slug');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('arteriaweb_bugshop_data');
+    }
+}
